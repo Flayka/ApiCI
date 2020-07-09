7ed9291a475809dfea0a54e7cf44993326fa86f4
Alexandra <rudakalexx@gmail.com>
2020-07-09 08:19:45 +0300

    Add Readme

---
+++ README.md
@@ -0,0 +1,1 @@
+**ApiCI**  [![Build status](https://ci.appveyor.com/api/projects/status/0bvtk4g818eqw9el?svg=true)](https://ci.appveyor.com/project/Flayka/apici)
--- src/main/java/ru/netology/rest/MobileBankApiTestV1.java
+++ src/main/java/ru/netology/rest/MobileBankApiTestV1.java
@@ -1,6 +1,10 @@
 package ru.netology.rest;
 
+import org.junit.jupiter.api.Test;
+import static io.restassured.RestAssured.given;
+
 class MobileBankApiTestV1 {
+
     @Test
     void shouldReturnDemoAccounts() {
         // Given - When - Then
